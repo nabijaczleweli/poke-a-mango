4583e57bfb340dd50ac6ff03fed65bfe2880c41c
nabijaczleweli <nabijaczleweli@gmail.com>
2016-10-12 23:44:42 +0200

    Basic UI framework

--- src/main-tui.rs
+++ src/main-tui.rs
@@ -2,8 +2,9 @@ extern crate poke_a_mango;
 extern crate cursive;
 
 use std::io::stderr;
-use std::process::exit;
 use cursive::Cursive;
+use std::process::exit;
+use cursive::views::{LinearLayout, Button};
 
 
 fn main() {
@@ -22,12 +23,27 @@ fn actual_main() -> i32 {
 
 fn result_main() -> Result<(), poke_a_mango::Error> {
     let opts = poke_a_mango::Options::parse();
-    println!("{:?}", opts);
 
     let mut ui = Cursive::new();
     ui.set_fps(60);
 
-    ui.add_global_callback('q', Cursive::quit);
+    let main_menu_screen = ui.add_screen();
+    let choose_difficulty_screen = ui.add_screen();
+    let highscores_screen = ui.add_screen();
+
+    ui.set_screen(highscores_screen);
+    // TODO: load the leaderboard
+    ui.add_layer(LinearLayout::vertical().child(Button::new("Back", move |ui| ui.set_screen(main_menu_screen))));
+
+    ui.set_screen(choose_difficulty_screen);
+    // TODO: difficulty selection
+    ui.add_layer(LinearLayout::vertical().child(Button::new("Back", move |ui| ui.set_screen(main_menu_screen))));
+
+    ui.set_screen(main_menu_screen);
+    ui.add_layer(LinearLayout::vertical()
+        .child(Button::new("Start game", move |ui| ui.set_screen(choose_difficulty_screen)))
+        .child(Button::new("Highscores", move |ui| ui.set_screen(highscores_screen)))
+        .child(Button::new("Exit", Cursive::quit)));
 
     ui.run();
 
